e4d791e11525aac94c20b328ef9e90192fc65357
Shin-jun <inside1451@naver.com>
2021-11-19 03:09:42 +0900

    16~20

---
+++ Python/venv/python100/python100.py
@@ -0,0 +1,58 @@
+"""
+1~5
+print("Hello")
+print("Hello World")
+print("Hello")
+print("World")
+print("'Hello'")
+print('"Hello World"')
+
+5~10
+print('\"!@#$%^&*()\'')
+print('\"C:\\Download\\\'hello\'.py\"')
+print('print(\"Hello\\nWorld\")')
+c = input()
+print(c)
+n = input()
+n = int(n)
+print(n)
+
+11~15
+f = input()
+f = float(f)
+print(f)
+
+a = input()
+b = input()
+print(b)
+print(a)
+
+c = input()
+c = float(c)
+print(c)
+print(c)
+print(c)
+
+d,e = input().split()
+print(d)
+print(e)
+
+
+
+#16~20
+a, b = input().split();
+print(b, a);
+
+a = input();
+print(a, a, a);
+
+a,b = input().split(":");
+print(a, b, sep=':');
+
+a,b,c = input().split('.');
+print(c,b,a,sep='-');
+
+a,b = input().split('-');
+print(a,b,sep='');
+
+"""
